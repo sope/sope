6347a07a9bae3a03141d1d55e326e700a2204ecd
a9 <a9@users.noreply.github.com>
2025-03-02 15:26:31 +0800

    feat(sonner-ts): initialize sonner-ts package with core components and configuration

---
+++ packages/sonner-ts/src/hooks.ts
@@ -0,0 +1,56 @@
+import { ref, watchEffect, type Ref } from 'vue'
+import { ToastState } from './state'
+import type { ToastT } from './types'
+
+export function useIsDocumentHidden() {
+  const isDocumentHidden = ref(false)
+
+  watchEffect(() => {
+    const callback = () => {
+      isDocumentHidden.value = document.hidden
+    }
+    document.addEventListener('visibilitychange', callback)
+    return () => window.removeEventListener('visibilitychange', callback)
+  })
+
+  return {
+    isDocumentHidden,
+  }
+}
+
+export function useVueSonner(): {
+  activeToasts: Ref<ToastT[]>
+} {
+  const activeToasts = ref<ToastT[]>([])
+
+  watchEffect((onInvalidate) => {
+    const unsubscribe = ToastState.subscribe((toast) => {
+      if ('dismiss' in toast && toast.dismiss) {
+        return activeToasts.value.filter((t) => t.id !== toast.id)
+      }
+
+      const existingToastIndex = activeToasts.value.findIndex(
+        (t) => t.id === toast.id,
+      )
+      if (existingToastIndex !== -1) {
+        const updatedToasts = [...activeToasts.value]
+        updatedToasts[existingToastIndex] = {
+          ...updatedToasts[existingToastIndex],
+          ...toast,
+        }
+
+        activeToasts.value = updatedToasts
+      } else {
+        activeToasts.value = [toast, ...activeToasts.value]
+      }
+    })
+
+    onInvalidate(() => {
+      unsubscribe()
+    })
+  })
+
+  return {
+    activeToasts,
+  }
+}
